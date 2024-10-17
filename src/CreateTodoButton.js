b35f3c46a7fb1506a0329371db18ea6ee88921c1
erickburgos <erickgburgos@gmail.com>
2024-10-17 17:30:54 -0300

    Agregue evento en todoseach, todobutton

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -3,8 +3,18 @@ import './CreateTodoButton.css'
 
 function CreateTodoButton () {
     return(
-        <button type="submit">Create task</button>
+        <button type="submit" 
+        onClick={(event) => {
+            console.log('Diste click')
+            console.log(event.target)
+            }}
+        >Create task</button>
     )
 }
+
+//PAra crear el onClick tiene que ir en camelcase y dentro de su etiqueta
+//ej. <button onClick={(event) => } creamo una arrow function para contener lo que queramos 
+
+//event representa el evento
 
 export { CreateTodoButton }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,9 +3,20 @@ import './App.css';
 
 function TodoSearch () {
     return (
-        <input className="input-search"placeholder="Cortar cebolla"/>
+        <input 
+        className="input-search"
+        placeholder="Cortar cebolla" 
+        onChange={(event) => {
+            console.log('Escribiste en todo search')
+            console.log(event.target)
+            console.log(event.target.value)
+         }
+        }
+        />
     )
 }
+
+//Podemos colocar el onChange
 
 
 export { TodoSearch }
